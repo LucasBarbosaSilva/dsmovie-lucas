45256f8c40a7f7aba3c5262b20a73627015d2533
Lucas Silva <blucas.oficial@gmail.com>
2022-12-27 18:44:12 -0300

    MovieCard

---
+++ frontend/src/components/MovieCard/index.tsx
@@ -0,0 +1,24 @@
+import { MovieScore } from "../MovieScore";
+import "./styles.css";
+function MovieCard() {
+  const movie = {
+    id: 1,
+    image: "https://cdn.ome.lt/wND2pNNHuSq-wcMgRmrD_XqM1-c=/1200x630/smart/extras/conteudos/o-espetacular-homem-aranha-2-promessas-nunca-cumpridas.jpg",
+    title: "O Espetacular Homem Aranha 2",
+    count: 2,
+    score: 4.5
+  }
+  return (
+      <div className="container-movie">
+          <img src={movie.image} alt="Imagem do filme" />
+          <div className="container-info">
+            <h3 id="title">
+              {movie.title}
+            </h3>
+            <MovieScore/>
+          </div>
+      </div>
+  );
+}
+
+export { MovieCard }
